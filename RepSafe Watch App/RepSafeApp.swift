18eaab44384bf753480528a83921c4e92aec1a01
izahai <ngynthhai@gmail.com>
2026-04-20 20:36:23 +0700

    Initial Commit

---
+++ RepSafe Watch App/RepSafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepSafeApp.swift
+//  RepSafe Watch App
+//
+//  Created by Hai Nguyen on 20/4/26.
+//
+
+import SwiftUI
+
+@main
+struct RepSafe_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
